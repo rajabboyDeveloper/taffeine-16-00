c9182c01096797b423a12fcf36e7442f035c0f21
rajabboy <coder@rajabboys-MacBook-Pro.local>
2023-12-19 16:50:37 +0500

    Navbar finished

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import l from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
 function Navbar() {
-  return <div>Navbar</div>;
+  const [val, setval] = useState([]);
+  useEffect(() => {
+    axios.get("https://admin.taffeine.com/api/menu").then((resp) => {
+      let data = resp.data.data;
+      setval(data);
+    });
+  }, []);
+
+  return (
+    <div className={l.navbar}>
+      <div className="container">
+        <nav className={l.nav}>
+          <a href="" className={l.logo}>
+            <img src="https://taffeine.com/_nuxt/img/logo.6335003.svg" alt="" />
+          </a>
+          <ul className={l.item}>
+            {val.map((items, index) => {
+              return (
+                <li className={l.item_list}>
+                  <NavLink key={index} to={items.slug} className={l.item_link}>
+                    {items.title}
+                  </NavLink>
+                </li>
+              );
+            })}
+          </ul>
+          <div className={l.btnBox}>
+            <button type="button" className={l.btn_one}>
+              Купить онлайн
+            </button>
+            <button type="button" className={l.btn_two}>
+              Русский
+            </button>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
